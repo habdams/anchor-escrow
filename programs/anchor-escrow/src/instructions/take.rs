ddce513177195302b0c2f4f277fdd178c8a95bfa
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2026-05-17 23:21:51 +0100

    update take instruction

--- programs/anchor-escrow/src/instructions/take.rs
+++ programs/anchor-escrow/src/instructions/take.rs
@@ -1,97 +1,98 @@
-use crate::state::Escrow;
-use crate::ESCROW_SEED;
 use anchor_lang::prelude::*;
 use anchor_spl::associated_token::AssociatedToken;
 use anchor_spl::token_interface::{
-    transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked,
+    close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface,
+    TransferChecked,
 };
 
+use crate::state::Escrow;
 #[derive(Accounts)]
-pub struct Taker<'info> {
+pub struct Take<'info> {
     #[account(mut)]
     pub taker: Signer<'info>,
 
     #[account(mut)]
     pub maker: SystemAccount<'info>,
 
-    #[account[
-		mint::token_program = token_program
-	]]
-    pub mint_a: InterfaceAccount<'info, Mint>,
+    #[account(mint::token_program = token_program)]
+    pub mint_a: Box<InterfaceAccount<'info, Mint>>,
 
-    #[account[
-		mint::token_program = token_program
-	]]
-    pub mint_b: InterfaceAccount<'info, Mint>,
+    #[account(mint::token_program = token_program)]
+    pub mint_b: Box<InterfaceAccount<'info, Mint>>,
 
-    #[account[
+    #[account(
+		init_if_needed,
+		payer = taker,
+		associated_token::mint = mint_a,
+		associated_token::authority = taker,
+		associated_token::token_program = token_program ,
+	)]
+    pub taker_ata_a: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+		init_if_needed,
+		payer = taker,
+		associated_token::mint = mint_b,
+		associated_token::authority = taker,
+		associated_token::token_program = token_program ,
+	)]
+    pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
 		init_if_needed,
 		payer = maker,
 		associated_token::mint = mint_b,
 		associated_token::authority = maker,
-		associated_token::token_program = token_program
-	]]
+		associated_token::token_program = token_program ,
+	)]
     pub maker_ata_b: InterfaceAccount<'info, TokenAccount>,
 
-    #[account[
-		init_if_needed,
-		payer = taker,
-		associated_token::mint = mint_a,
-		associated_token::authority = taker,
-		associated_token::token_program = token_prgram
-	]]
-    pub taker_ata_a: InterfaceAccount<'info, TokenAccount>,
-
-    #[account[
-		init_if_needed,
-		payer = taker,
-		associated_token::mint = mint_b,
-		associated_token::authority = taker,
-		associated_token::token_program = token_prgram
-	]]
-    pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
-
-    #[account[
+    #[account(
 		mut,
 		close = maker,
-		seeds = [ESCROW_SEED, escrow.maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+		seeds = [b"escrow", escrow.maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
 		bump,
 		has_one = mint_a,
 		has_one = mint_b,
-		has_one = maker
-	]]
+		has_one = maker,
+	)]
     pub escrow: Account<'info, Escrow>,
 
-    #[account[
+    #[account(
 		mut,
 		associated_token::mint = mint_a,
 		associated_token::authority = escrow,
-		associated_token::token_program = token_prgram
-	]]
+		associated_token::token_program = token_program
+	)]
     pub vault: InterfaceAccount<'info, TokenAccount>,
 
     pub token_program: Interface<'info, TokenInterface>,
+
     pub associated_token_program: Program<'info, AssociatedToken>,
+
     pub system_program: Program<'info, System>,
 }
 
 impl<'info> Take<'info> {
-    // Transfer token from the taker to maker
+    /**
+     * Transfer the tokens from taker to maker
+     */
     pub fn deposit(&mut self) -> Result<()> {
         let cpi_accounts = TransferChecked {
-            from: self.taker_ata_a.to_account_info(),
+            from: self.taker_ata_b.to_account_info(),
             mint: self.mint_b.to_account_info(),
             to: self.maker_ata_b.to_account_info(),
             authority: self.taker.to_account_info(),
         };
 
-        let cpi_ctx = CpiContext::new(self.token_progam.key(), cpi_accounts);
+        let cpi_ctx = CpiContext::new(self.token_program.key(), cpi_accounts);
 
         transfer_checked(cpi_ctx, self.escrow.receive, self.mint_b.decimals)
     }
 
-    // Withdraw token from the vault to taker and close the vault
-
+    /**
+     * Withdraw tokens from the vault to the taker and close the vault
+     */
     pub fn withdraw_and_close_vault(&mut self) -> Result<()> {
         let cpi_accounts = TransferChecked {
             from: self.vault.to_account_info(),
@@ -103,14 +104,14 @@ impl<'info> Take<'info> {
         let seed_bytes = self.escrow.seed.to_le_bytes();
 
         let signer_seeds: &[&[&[u8]]] = &[&[
-            ESCROW_SEED,
+            b"escrow",
             self.escrow.maker.as_ref(),
             seed_bytes.as_ref(),
             &[self.escrow.bump],
         ]];
 
         let cpi_ctx =
-            CpiContext::new_with_signer(self.token_progam.key(), cpi_accounts, signer_seeds);
+            CpiContext::new_with_signer(self.token_program.key(), cpi_accounts, signer_seeds);
 
         transfer_checked(cpi_ctx, self.escrow.receive, self.mint_b.decimals)?;
 
@@ -119,10 +120,8 @@ impl<'info> Take<'info> {
             destination: self.maker.to_account_info(),
             authority: self.escrow.to_account_info(),
         };
-
-        let cpi_ctx =
-            CpiContext::new_with_signer(self.token_progam.key(), cpi_accounts, &signer_seeds);
-
-        close_account(cpi_ctx)
+        let cpi_context =
+            CpiContext::new_with_signer(self.token_program.key(), cpi_accounts, signer_seeds);
+        close_account(cpi_context)
     }
 }
