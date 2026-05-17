b57635afd7be463e0ac9d501b8dba4b7b795f7d5
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2026-05-17 23:20:57 +0100

    Add instruction for Refund

--- programs/anchor-escrow/src/lib.rs
+++ programs/anchor-escrow/src/lib.rs
@@ -3,8 +3,6 @@ pub mod error;
 pub mod instructions;
 pub mod state;
 
-use anchor_lang::prelude::*;
-
 pub use constants::*;
 pub use instructions::*;
 pub use state::*;
@@ -16,14 +14,19 @@ pub mod anchor_escrow {
     use super::*;
 
     #[instruction(discriminator = 0)]
-    pub fn make(ctx: Context<Make>, seed: u64, recieve: u64, deposit: u64) -> Result<()> {
+    pub fn make(ctx: Context<Make>, seed: u64, receive: u64, deposit: u64) -> Result<()> {
         ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
-        ctx.account.deposit(deposit)
+        ctx.accounts.deposit(deposit)
     }
 
     #[instruction(discriminator = 1)]
-    pub fn take(ctx: Context<Make>) -> Result<()> {
+    pub fn take(ctx: Context<Take>) -> Result<()> {
         ctx.accounts.deposit()?;
-        ctx.account.withdraw_and_close_vault()
+        ctx.accounts.withdraw_and_close_vault()
+    }
+
+    #[instruction(discriminator = 2)]
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
     }
 }
